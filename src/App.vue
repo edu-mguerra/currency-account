257fe7a0083a183bfa0741b557a4d29e7a514191
Eduardo Guerra <eduguerra.dev@gmail.com>
2024-08-09 12:46:12 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<script setup>
+import cot from './components/cot.vue';
+import api from './serve/api';
+import { onMounted, reactive, toRefs } from 'vue';
+
+
+const data = reactive({ 
+  quotes: {},
+
+})
+const { quotes } = toRefs(data);
+
+onMounted(async () => {
+  const response = await api.all()
+  data.quotes = response.data
+})
+
+</script>
+
+<template>
+  <div class="container grid-lg my-2  py-2 card1">
+    <div class="card">
+
+      <div class="card-header">
+
+        <div class="h4 text-center">Exibindo moedas</div>
+
+      </div>
+
+      <div class="card-body">
+        <cot :quotes="quotes" />
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card1 {
+
+  align-items: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+@media ((min-width: 320px) and (max-width: 426px)) {
+  .card {
+    font-size: 10px;
+    gap: 0;
+    margin-left: 20px;
+
+  }
+}
+
+.h4 {
+  margin-bottom: 10px;
+}
+
+.card {
+  box-shadow: inset 0 0 1em gold, 0 0 1em red;
+}
+</style>
---
+++ src/components/cot.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    quotes: {
+        type: Object,
+        required: true
+    }
+})
+
+</script>
+
+<template>
+    <div class="table">
+        <thead>
+
+            <tr>
+                <th>Código:</th>
+                <th>Nome:</th>
+                <th>Máximo:</th>
+                <th>Minimo:</th>
+                <th>Variação:</th>
+                <th></th>
+            </tr>
+
+        </thead>
+        <tbody>
+
+            <tr v-for="(quote, key) in props.quotes" :key="key">
+
+                <td>{{ key }}</td>
+                <td>{{ quote.name }}</td>
+                <td>{{ quote.high }}</td>
+                <td>{{ quote.low }}</td>
+
+                <td>
+                    <span
+                    class="label label-rounded text-small"
+                    :class="{'label-error' : quote.pctChange < 0, 'label-success' : quote.pctChange > 0}"
+                    >
+
+                        > {{ quote.pctChange }} %
+                    </span>
+                </td>
+
+
+            </tr>
+
+        </tbody>
+    </div>
+</template>
+
+<style scoped></style>
